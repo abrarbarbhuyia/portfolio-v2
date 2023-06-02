3156a8a5e41456e38ddce84fa15bc629ba92bd02
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-107.tpgi.com.au>
2023-06-02 17:22:15 +1000

    fix: removed unused import

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import FadeInWhenVisible from "./FadeInWhenVisible";
 import Image from "next/image";
 
 interface CompanyExperienceProps {

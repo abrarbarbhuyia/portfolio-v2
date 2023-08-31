9600be81fd78370b42dcd59e593608c1cdccf65b
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-103.tpgi.com.au>
2023-08-31 23:05:12 +1000

    fix: removed variants

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,17 +4,17 @@ import { Grid, Typography, Paper, Box } from "@mui/material";
 const Skills = () => {
   return (
     <div className="m-8">
-      <Typography variant="h4" className="mb-6 font-bold text-xl">
+      <Typography  className="mb-6 font-bold text-xl">
         Technical Skills
       </Typography>
       <Grid container spacing={3}>
         {/* Containerization */}
         <Grid item xs={12} md={4}>
           <Paper elevation={3} className="p-4 h-full">
-            <Typography variant="h5" className="mb-2 font-bold text-lg">
+            <Typography className="mb-2 font-bold text-lg">
               🐳 Containerization
             </Typography>
-            <Typography variant="body1">
+            <Typography >
               Docker: Experience with creating and managing containers.
               <br />Kubernetes: Familiarity with container orchestration.
             </Typography>
@@ -24,10 +24,10 @@ const Skills = () => {
         {/* Programming Languages */}
         <Grid item xs={12} md={4}>
           <Paper elevation={3} className="p-4 h-full ">
-            <Typography variant="h5" className="mb-2 font-bold text-lg">
+            <Typography className="mb-2 font-bold text-lg">
               💻 Programming Languages
             </Typography>
-            <Typography variant="body1">
+            <Typography >
               Python, Java, JavaScript, Typescript, C++, C#, HTML/CSS, YAML
             </Typography>
           </Paper>
@@ -36,10 +36,10 @@ const Skills = () => {
         {/* Technologies */}
         <Grid item xs={12} md={4}>
           <Paper elevation={3} className="p-4 h-full">
-            <Typography variant="h5" className="mb-2 font-bold text-lg">
+            <Typography className="mb-2 font-bold text-lg">
               🛠️ Technologies/Tools
             </Typography>
-            <Typography variant="body1">
+            <Typography >
               Node.js, Next.js, React.js, SQL, Git, APIs, MongoDB, Grafana, ArgoCD, GraphQL, Ansible, Jira, Trello, Confluence
             </Typography>
           </Paper>
@@ -47,38 +47,38 @@ const Skills = () => {
       </Grid>
 
       <Box mt={6}>
-        <Typography variant="h4" className="mb-6 font-bold text-xl">
+        <Typography  className="mb-6 font-bold text-xl">
           Transferable Skills
         </Typography>
         <Paper elevation={3} className="p-4">
-          <Typography variant="h5" className="mb-2 font-bold text-lg">
+          <Typography className="mb-2 font-bold text-lg">
             🚀 Leadership
           </Typography>
-          <Typography variant="body1">
+          <Typography >
             Developed leadership skills by leading the migration of an enterprise Gitlab instance to ArgoCD. In a team of three engineers, I was responsible for the planning, execution, and documentation of the migration.
           </Typography>
-          <Typography variant="h5" className="mt-4 mb-2 font-bold text-lg">
+          <Typography className="mt-4 mb-2 font-bold text-lg">
             🗣️ Communication
           </Typography>
-          <Typography variant="body1">
+          <Typography >
             Took ownership of critical production issues that blocked multiple high-priority projects. I communicated with stakeholders and engineers to provide estimates and updates on the status of the issues.
           </Typography>
-          <Typography variant="h5" className="mt-4 mb-2 font-bold text-lg">
+          <Typography className="mt-4 mb-2 font-bold text-lg">
             🧩 Problem Solving
           </Typography>
-          <Typography variant="body1">
+          <Typography >
             Fixed issues around the repoint of a production API URL by researching the issue, trial and error, and utilizing documentation. I also implemented a long-term solution to prevent the issue from reoccurring.
           </Typography>
-          <Typography variant="h5" className="mt-4 mb-2 font-bold text-lg">
+          <Typography className="mt-4 mb-2 font-bold text-lg">
             ⏳ Time Management
           </Typography>
-          <Typography variant="body1">
+          <Typography >
             Enhanced time-management skills by using Jira ticket estimation to plan and prioritize tasks. I also used Trello to manage my personal tasks and projects.
           </Typography>
-          <Typography variant="h5" className="mt-4 mb-2 font-bold text-lg">
+          <Typography className="mt-4 mb-2 font-bold text-lg">
             🧠 Critical Thinking
           </Typography>
-          <Typography variant="body1">
+          <Typography >
             Subverted the views of senior engineers by providing a different perspective on the issue, where I used new technologies to solve a long standing problem that was originally dealt with manaully.
           </Typography>
         </Paper>

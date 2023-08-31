743485c097b4748d667765aeb01bd3b294a5da84
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-103.tpgi.com.au>
2023-08-31 23:45:35 +1000

    fix: removing variant

--- src/components/AcademicExperience.tsx
+++ src/components/AcademicExperience.tsx
@@ -71,139 +71,117 @@ const AcademicExperience = () => {
   };
 
   return (
-      <div className="m-8">
-        <Box height="350px">
-          <Paper elevation={3} className="p-5" style={{ height: "100%" }}>
-            {activeExperience.type === "University Info" ? (
-              <>
-                <p
-                  variant="h6"
-                  className="text-lg font-bold text-gray-900 md:text-xl"
-                >
-                  {activeExperience.university}
-                </p>
-                <div className="text-sm font-semibold text-gray-500 md:mt-0.5 md:text-base">
-                  {activeExperience.degree}
-                </div>
-                <div className="text-sm text-gray-500 md:mt-0.5 md:text-base">
-                  {activeExperience.year}
-                </div>
-                {activeExperience.achievements &&
-                  activeExperience.achievements.length > 0 && (
-                    <div className="mt-4">
-                      <p
-                        variant="subtitle2"
-                        className="text-lg font-bold text-gray-900"
-                      >
-                        Achievements:
-                      </p>
-                      <ul className="mt-2 list-disc space-y-2 text-sm text-gray-900 md:pl-6 md:text-base">
-                        {activeExperience.achievements.map(
-                          (achievement, index) => (
-                            <li key={index} className="ml-4">
-                              {achievement}
-                            </li>
-                          )
-                        )}
-                      </ul>
-                    </div>
-                  )}
-                {activeExperience.involvement &&
-                  activeExperience.involvement.length > 0 && (
-                    <div className="mt-4">
-                      <p
-                        variant="subtitle2"
-                        className="text-lg font-bold text-gray-900"
-                      >
-                        Involvement:
-                      </p>
-                      <ul className="mt-2 list-disc space-y-2 text-sm text-gray-900 md:pl-6 md:text-base">
-                        {activeExperience.involvement.map(
-                          (involvement, index) => (
-                            <li key={index} className="ml-4">
-                              {involvement}
-                            </li>
-                          )
-                        )}
-                      </ul>
-                    </div>
-                  )}
-              </>
-            ) : (
-              <div>
-                <p
-                  variant="h6"
-                  className="text-lg font-bold text-gray-900 md:text-xl"
-                >
-                  {activeExperience.title}
-                </p>
-                <p variant="body1" paragraph>
-                  {activeExperience.description}
-                </p>
-                <p
-                  variant="subtitle2"
-                  className="text-lg font-bold text-gray-900"
-                >
-                  Technologies Used:
-                </p>
-                <ul className="mt-2 list-disc space-y-2 text-sm text-gray-900 md:pl-6 md:text-base">
-                  {activeExperience.technologies?.map((technology, index) => (
-                    <li key={index} className="ml-4">
-                      {technology}
-                    </li>
-                  ))}
-                </ul>
-                {activeExperience.githubLink && (
+    <div className="m-8">
+      <Box height="350px">
+        <Paper elevation={3} className="p-5" style={{ height: "100%" }}>
+          {activeExperience.type === "University Info" ? (
+            <>
+              <p className="text-lg font-bold text-gray-900 md:text-xl">
+                {activeExperience.university}
+              </p>
+              <div className="text-sm font-semibold text-gray-500 md:mt-0.5 md:text-base">
+                {activeExperience.degree}
+              </div>
+              <div className="text-sm text-gray-500 md:mt-0.5 md:text-base">
+                {activeExperience.year}
+              </div>
+              {activeExperience.achievements &&
+                activeExperience.achievements.length > 0 && (
                   <div className="mt-4">
-                    <p
-                      variant="subtitle2"
-                      className="text-lg font-bold text-gray-900"
-                    >
-                      GitHub Link:
+                    <p className="text-lg font-bold text-gray-900">
+                      Achievements:
                     </p>
-                    <Link
-                      href={activeExperience.githubLink}
-                      target="_blank"
-                      rel="noopener"
-                      className="text-blue-500"
-                    >
-                      {activeExperience.githubLink}
-                    </Link>
+                    <ul className="mt-2 list-disc space-y-2 text-sm text-gray-900 md:pl-6 md:text-base">
+                      {activeExperience.achievements.map(
+                        (achievement, index) => (
+                          <li key={index} className="ml-4">
+                            {achievement}
+                          </li>
+                        )
+                      )}
+                    </ul>
                   </div>
                 )}
-              </div>
-            )}
-          </Paper>
-        </Box>
-        <div className="custom-carousel-buttons">
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={activeStep === 0}
-            onClick={goToPrevStep}
-            startIcon={<FaArrowLeft />}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={activeStep === academicExperiences.length - 1}
-            onClick={goToNextStep}
-            endIcon={<FaArrowRight />}
-          >
-            Next
-          </Button>
-        </div>
-        <style jsx>{`
-          .custom-carousel-buttons {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-top: 10px;
-          }
-        `}</style>
+              {activeExperience.involvement &&
+                activeExperience.involvement.length > 0 && (
+                  <div className="mt-4">
+                    <p className="text-lg font-bold text-gray-900">
+                      Involvement:
+                    </p>
+                    <ul className="mt-2 list-disc space-y-2 text-sm text-gray-900 md:pl-6 md:text-base">
+                      {activeExperience.involvement.map(
+                        (involvement, index) => (
+                          <li key={index} className="ml-4">
+                            {involvement}
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  </div>
+                )}
+            </>
+          ) : (
+            <div>
+              <p className="text-lg font-bold text-gray-900 md:text-xl">
+                {activeExperience.title}
+              </p>
+              <p>{activeExperience.description}</p>
+              <p className="text-lg font-bold text-gray-900">
+                Technologies Used:
+              </p>
+              <ul className="mt-2 list-disc space-y-2 text-sm text-gray-900 md:pl-6 md:text-base">
+                {activeExperience.technologies?.map((technology, index) => (
+                  <li key={index} className="ml-4">
+                    {technology}
+                  </li>
+                ))}
+              </ul>
+              {activeExperience.githubLink && (
+                <div className="mt-4">
+                  <p className="text-lg font-bold text-gray-900">
+                    GitHub Link:
+                  </p>
+                  <Link
+                    href={activeExperience.githubLink}
+                    target="_blank"
+                    rel="noopener"
+                    className="text-blue-500"
+                  >
+                    {activeExperience.githubLink}
+                  </Link>
+                </div>
+              )}
+            </div>
+          )}
+        </Paper>
+      </Box>
+      <div className="custom-carousel-buttons">
+        <Button
+          color="primary"
+          disabled={activeStep === 0}
+          onClick={goToPrevStep}
+          startIcon={<FaArrowLeft />}
+        >
+          Previous
+        </Button>
+        <Button
+          color="primary"
+          disabled={activeStep === academicExperiences.length - 1}
+          onClick={goToNextStep}
+          endIcon={<FaArrowRight />}
+        >
+          Next
+        </Button>
       </div>
+      <style jsx>{`
+        .custom-carousel-buttons {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-top: 10px;
+        }
+      `}</style>
+    </div>
   );
 };
 

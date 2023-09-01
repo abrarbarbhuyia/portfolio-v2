954c1bacfed2e639902b1d4630588b8860ab3b20
Abrar Barbhuyia <abrarbarbhuyia@Abrars-Macbook-Pro-Home.local>
2023-09-01 13:34:15 +1000

    feat: added button to make clicking nicer

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,30 @@
 import React from "react";
-import { List, ListItem, ListItemText, Box } from "@mui/material";
+import { List, ListItem, ListItemText } from "@mui/material";
 
 const Sidebar = () => {
   return (
-    <Box>
-      <nav className="sticky top-0 h-screen p-4 text-white">
-        <List>
-          <ListItem component="a" href="#home">
-            <ListItemText primary="Home" />
-          </ListItem>
-          <ListItem component="a" href="#career">
-            <ListItemText primary="Career" />
-          </ListItem>
-          <ListItem component="a" href="#academic-experience">
-            <ListItemText primary="Academic Experience" />
-          </ListItem>
-          <ListItem component="a" href="#leadership">
-            <ListItemText primary="Leadership" />
-          </ListItem>
-          <ListItem component="a" href="#teamwork">
-            <ListItemText primary="Teamwork" />
-          </ListItem>
-          <ListItem component="a" href="#skills">
-            <ListItemText primary="Skills" />
-          </ListItem>
-          <ListItem component="a" href="#resume">
-            <ListItemText primary="Resume" />
-          </ListItem>
-        </List>
-      </nav>
-    </Box>
+    <nav className="sticky top-0 h-screen text-white p-4">
+      <List>
+        <ListItem button component="a" href="#home">
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button component="a" href="#career">
+          <ListItemText primary="Career" />
+        </ListItem>
+        <ListItem button component="a" href="#academic-experience">
+          <ListItemText primary="Academic Experience" />
+        </ListItem>
+        <ListItem button component="a" href="#leadership">
+          <ListItemText primary="Leadership" />
+        </ListItem>
+        <ListItem button component="a" href="#teamwork">
+          <ListItemText primary="Teamwork" />
+        </ListItem>
+        <ListItem button component="a" href="#skills">
+          <ListItemText primary="Skills" />
+        </ListItem>
+      </List>
+    </nav>
   );
 };
 

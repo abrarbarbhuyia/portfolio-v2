2a5a2f91e0f3104fe0ea2a44d8834061f9344b56
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-105.tpgi.com.au>
2023-10-06 22:39:05 +1100

    fix: fixing academic experience title

--- src/components/AcademicExperienceTitle.tsx
+++ src/components/AcademicExperienceTitle.tsx
@@ -34,18 +34,23 @@ const AcademicExperienceTitle = () => {
       </h2>
       <div className="md:mb-6 md:border-b-2 md:border-white"></div>
       <p className="text-xl font-bold text-white md:text-2xl">
-       Experience in academia.
+        Experience in academia.
       </p>
       <p className="mt-4 text-white">
         Through my Academic Experience and Projects, I&apos;ve had the chance to
         learn and apply my skills to realistic projects. By building projects
         from scratch, I know how to prioritise the needs of stakeholders and how
-        to deliver a completed product in a timely manner. These skills allow me
-        to be a valuable asset in any company, as I can apply my technical
-        skills while having a business understanding of the project I am working
-        on. The value I provide to a company is my ability to learn and adapt to
-        new technologies and environments, and my ability to communicate with
-        stakeholders and other team members.
+        to deliver a completed product in a timely manner.
+      </p>
+      <p className="text-w mt-4">
+        These skills allow me to be a valuable asset in any company, as I can
+        apply my technical skills and communication skills while having a
+        business understanding of the project or team I am working on.
+      </p>
+
+      <p className="text-w mt-4">
+        I aim to continue to develop my technical skills further through
+        academic experience and projects, which is showcased below:
       </p>
     </motion.div>
   );
